485f337bd8aead22ef971ff83cd92f1c1385eea5
Bruno Casas <brunocasas04@gmail.com>
2021-11-15 22:13:08 -0400

    Remove log

--- app/middleware/exceptionMiddleware.js
+++ app/middleware/exceptionMiddleware.js
@@ -2,7 +2,6 @@ import { HttpError } from '../errors'
 import { makeResponse } from '../utils'
 
 export function exceptionMiddlewere(err, req, res, next) {
-    console.log(err);
     if (err instanceof HttpError) {
         res.status(err.httpCode).json(makeResponse(err.message, false));
     } else {
